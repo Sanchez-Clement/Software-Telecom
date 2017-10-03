41305b6f8f0356210b644d69b535042efc9c6eb0
Sanchez-Clement <sanchez.clmnt@gmail.com>
2017-10-03 11:40:32 +0200

    Update connexion_sql.php

--- modele/connexion_sql.php
+++ modele/connexion_sql.php
@@ -3,7 +3,7 @@
 <?php
 function connect() {
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=telecom;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=localhost;dbname=telecom;charset=utf8', '***', '***', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
     die('Erreur : '.$e->getMessage());
 }
